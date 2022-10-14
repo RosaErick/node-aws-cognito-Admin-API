a33d426e177255ef40f752d48ad3addcd779963c
Erick Rosa <erickpmotta@gmail.com>
2022-10-14 19:39:03 -0300

    add try/catch to controller functions

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,43 +5,71 @@ class AuthController {
     this.cognito = new Cognito();
   }
   async signIn(req, res) {
-    const { username, password } = req.body;
-    const result = await this.cognito.login(username, password);
-    res.json(result);
+    try {
+      const { username, password } = req.body;
+      const result = await this.cognito.login(username, password);
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+    }
   }
 
   async signUp(req, res) {
-    const { username, password } = req.body;
-    const result = await this.cognito.register(username, password);
-    res.json(result);
-  }
-
-  async signOut(req, res) {
-    const result = await this.cognito.logout();
-    res.json(result);
+    try {
+      const { username, password } = req.body;
+      const result = await this.cognito.register(username, password);
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+    }
   }
 
   async addUserInGroup(req, res) {
-    const { username, groupname } = req.body;
-    const result = await this.cognito.addUserInGroup(username, groupname);
-    res.json(result);
+    try {
+      const { username, groupname } = req.body;
+      const result = await this.cognito.addUserInGroup(username, groupname);
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+    }
   }
 
   async removeUserFromGroup(req, res) {
-    const { username, groupname } = req.body;
-    const result = await this.cognito.removeUserFromGroup(username, groupname);
-    res.json(result);
+    try {
+      const { username, groupname } = req.body;
+      const result = await this.cognito.removeUserFromGroup(
+        username,
+        groupname
+      );
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+    }
   }
 
   async lisUsers(req, res) {
-    const result = await this.cognito.listUsers();
-    res.json(result);
+    try {
+      const result = await this.cognito.listUsers();
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+    }
   }
 
   async updateAttributes(req, res) {
-    const { username, attributes } = req.body;
-    const result = await this.cognito.updateAttributes(username, attributes);
-    res.json(result);
+    try {
+      const { username, attributes } = req.body;
+      const result = await this.cognito.updateAttributes(username, attributes);
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+    }
   }
 }
 
