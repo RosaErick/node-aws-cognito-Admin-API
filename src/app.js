2b5cc215f4d8c9891ef28ad4a7e1696dc6cdc947
Erick Rosa <erickpmotta@gmail.com>
2022-10-14 19:33:53 -0300

    fixing routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes";
 
 class App {
   constructor() {
     this.server = express();
-
     this.middlewares();
+    this.routes();
   }
 
   middlewares() {
@@ -13,7 +14,9 @@ class App {
     this.server.use(express.json());
   }
 
-  routes() {}
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
